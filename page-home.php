19210aaa77edea3e7c4271e8d2a1998e1a52e149
mtorresosmos <mtorres@somososmos.com>
2024-07-27 13:34:03 -0600

    OS - Header and footer mobiles, fixes on home

--- page-home.php
+++ page-home.php
@@ -6,7 +6,7 @@
 
 	<main role="main" aria-label="Content" class="home">
 		<div class="video-section">
-			<video autoplay muted>
+			<video autoplay muted loop>
 				<source src="<?php echo get_template_directory_uri();?>/video/video-home.mp4" type="video/mp4">
 				El navegador no soporta la etiqueta de video.
 			</video>

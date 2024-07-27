6e92d2722f736a9acbc516d430933f55406e3428
mtorresosmos <mtorres@somososmos.com>
2024-07-27 13:41:52 -0600

    OS - Master update

---
+++ page-home.php
@@ -0,0 +1,25 @@
+<?php 
+	/* Template Name: Home */
+
+	get_header(); 
+?>
+
+	<main role="main" aria-label="Content" class="home">
+		<div class="video-section home-section">
+			<video autoplay muted>
+				<source src="<?php echo get_template_directory_uri();?>/video/video-home.mp4" type="video/mp4">
+				El navegador no soporta la etiqueta de video.
+			</video>
+		</div>
+
+		<div class="projects-home">
+			<div class="content-wrapper home-section">
+				<h1 class="bg-title">
+					<span>Let's see some work</span>
+				</h1>
+			</div>
+			<?php get_template_part( 'template-parts/carousels/carousel-home-projects' ); ?>
+		</div>
+	</main>
+
+<?php get_footer(); ?>
